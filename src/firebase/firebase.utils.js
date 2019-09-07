d061aa7fe8c90146a58512e0305a2bfef8651523
paula <paulagrabiela.anez@students.salle.url.edu>
2019-09-07 21:52:45 +0200

    Implemented firebase utils,store authenticated users on firebase and created signUp component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,10 +7,37 @@ const config = {
     authDomain: "crwn-db-c894b.firebaseapp.com",
     databaseURL: "https://crwn-db-c894b.firebaseio.com",
     projectId: "crwn-db-c894b",
-    storageBucket: "",
+    storageBucket: "crwn-db-c894b.appspot.com",
     messagingSenderId: "723603147604",
     appId: "1:723603147604:web:f0a978aa4838af5a"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        
+        //create new user,using the data from the userAuth object
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    console.log(snapShot);
+    return userRef;
+}
+ 
 
 firebase.initializeApp(config);
 
